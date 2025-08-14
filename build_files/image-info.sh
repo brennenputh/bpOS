d2ad25d02ca725db946d06931cba546a496e5bb2
brennenputh <brennenputh@gmail.com>
2025-08-14 19:46:27 -0400

    Set the icons better - and prepare ISO build

--- build_files/image-info.sh
+++ build_files/image-info.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 IMAGE_PRETTY_NAME="bpOS"
-IMAGE_NAME="bpos"
-IMAGE_LIKE="fedora"
 
-sed -i "s|^VARIANT_ID=.*|VARIANT_ID=$IMAGE_NAME|" /usr/lib/os-release
 sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"${IMAGE_PRETTY_NAME}\"|" /usr/lib/os-release
 sed -i "s|^NAME=.*|NAME=\"$IMAGE_PRETTY_NAME\"|" /usr/lib/os-release
-sed -i "s|^ID=fedora|ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"${IMAGE_LIKE}\"|" /usr/lib/os-release
-echo "IMAGE_ID=\"${IMAGE_NAME}\"" >> /usr/lib/os-release
-
-# Fix issues caused by ID no longer being fedora
-sed -i "s|^EFIDIR=.*|EFIDIR=\"fedora\"|" /usr/sbin/grub2-switch-to-blscfg
-
