080e236854bf5b0771ab0ef5ad9e62a17e00a978
brennenputh <brennenputh@gmail.com>
2026-01-04 11:11:29 -0500

    YOLO stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,5 +6,9 @@ set -ouex pipefail
 dnf5 install -y \
   alacritty
 
+# Brother Printer Drivers
+dnf5 install -y \
+  /rpms/hll2395dwpdrv-4.0.0-1.i386.rpm
+
 # Copy system files to container
 rsync -rvK /ctx/system_files/ /
