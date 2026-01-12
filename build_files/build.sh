c4a731a3d007096c772ac150c583e7e7ccc68fb1
brennenputh <brennenputh@gmail.com>
2026-01-12 08:12:36 -0500

    Add GlobalProtect to image

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,5 +12,10 @@ dnf5 install -y \
   http://download.brother.com/welcome/dlf103563/hll2395dwpdrv-4.0.0-1.i386.rpm
 rm /etc/rpm/macros.verify
 
+# GlobalProtect
+dnf5 copr enable -y yuezk/globalprotect-openconnect
+dnf5 install -y globalprotect-openconnect
+dnf5 copr disable -y yuezk/globalprotect-openconnect
+
 # Copy system files to container
 rsync -rvK /ctx/system_files/ /
