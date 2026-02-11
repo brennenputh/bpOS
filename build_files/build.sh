505c6d165d968e2c8270edb3d8f0001f88f6eb93
brennenputh <brennenputh@gmail.com>
2026-02-11 08:13:47 -0500

    Remove broken printer drivers

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,12 +6,6 @@ set -ouex pipefail
 dnf5 install -y \
   alacritty
 
-# Brother Printer Drivers
-echo "%_pkgverify_level none" >/etc/rpm/macros.verify
-dnf5 install -y \
-  http://download.brother.com/welcome/dlf103563/hll2395dwpdrv-4.0.0-1.i386.rpm
-rm /etc/rpm/macros.verify
-
 # GlobalProtect
 dnf5 copr enable -y yuezk/globalprotect-openconnect
 dnf5 install -y globalprotect-openconnect
