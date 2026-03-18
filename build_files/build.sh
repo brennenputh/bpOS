9888b739df15a6e2e5589534f83e7c9ef01d505b
brennenputh <bputh.blog@gmail.com>
2026-03-18 09:24:23 -0400

    Attempt to fix build

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,10 @@
 
 set -ouex pipefail
 
-# Base System Packages
-dnf5 install -y \
-  alacritty
-
 # GlobalProtect
-dnf5 copr enable -y yuezk/globalprotect-openconnect
-dnf5 install -y globalprotect-openconnect
-dnf5 copr disable -y yuezk/globalprotect-openconnect
+dnf copr enable -y yuezk/globalprotect-openconnect
+dnf install -y globalprotect-openconnect
+dnf copr disable -y yuezk/globalprotect-openconnect
 
 # Copy system files to container
 rsync -rvK /ctx/system_files/ /
